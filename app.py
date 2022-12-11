96255f26fdeb78bd0edda2c04641312769c134b0
Carl Thomas <carl.thomas@postman.com@CarlsWorkMBP2-2.lan>
2022-12-11 14:07:36 -0600

    First Commit into git!

---
+++ app.py
@@ -0,0 +1,114 @@
+import os
+import secrets
+
+from flask import Flask, jsonify
+from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+
+#importing DB models
+from db import db
+from blocklist import BLOCKLIST
+import models
+
+#importing blueprints
+from resources.user import blp as UserBlueprint
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    #app config files
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    api = Api(app)
+
+    #JWT token
+    app.config["JWT_SECRET_KEY"] = "111002764187488216573489325415958554413"
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        #Should ideally be looking into a db to determin an admin
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    #Error handling for JWT tokens
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
+    #will be creating tables in db if they do not already exist
+    #can delete since using flask-migrate now
+    # with app.app_context():
+    #     db.create_all()
+
+    #registering blueprints for store and items
+    api.register_blueprint(UserBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+
+    return app
---
+++ resources/item.py
@@ -0,0 +1,72 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from flask_jwt_extended import jwt_required, get_jwt
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
+
+blp = Blueprint("items", __name__, description="Operations on items")
+
+@blp.route("/item/<int:item_id>")
+class Item(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema)
+
+    #this will find a specific item
+    def get(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+
+    #this will delete a item
+    @jwt_required()
+    def delete(self, item_id):
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin priviledge required")
+
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted successfully."}, 200
+
+    #this will update a specific item
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+        
+        db.session.add(item)
+        db.session.commit()
+
+        return item
+
+
+@blp.route("/item")
+class ItemList(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema(many=True))
+    #this will get all items
+    def get(self):
+        return ItemModel.query.all()
+
+    @jwt_required(fresh=True)
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    #this will create a new item
+    def post(self, item_data):
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error has occurred while inserting the item.")
+        
+        return item
